b20da9638e1a06610761b0dd79daa82e9a8c4ab2
Daniel Nagy <danielnagy589@gmail.com>
2026-05-18 22:25:55 +0300

    added offline cache

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart' hide AuthProvider;
 import 'package:likealocal/home/pages/explore_page.dart';
@@ -18,6 +19,10 @@ void main() async {
 
   try {
     await Firebase.initializeApp();
+    FirebaseFirestore.instance.settings = const Settings(
+      persistenceEnabled: true,
+      cacheSizeBytes: Settings.CACHE_SIZE_UNLIMITED,
+    );
     await MessageNotificationService.instance.initialize();
 
     FirebaseAuth.instance.authStateChanges().listen((user) {
